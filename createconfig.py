d1d7b3d2cddf03b57ca38f85c02d8b0a796c4294
Thomas Gerum <tgerum@googlemail.com>
2023-10-04 18:01:23 +0200

    Hardcoded Capturerechtecke erstellt

--- createconfig.py
+++ createconfig.py
@@ -1,7 +1,7 @@
 import configparser
 
 config = configparser.ConfigParser()
-config["InterfacesToUse"] = {"Input": "testone", "Output": "testout"}
-config["Frequency"] = {"PollingRate": 5}
+config["InterfacesToUse"] = {"Input": "captureWebcam", "Output": "testout"}
+config["Frequency"] = {"PollingRate": 3}
 with open("config.ini", "w") as configfile:
     config.write(configfile)
